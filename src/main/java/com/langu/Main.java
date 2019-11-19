41b25e6b1123de01d7ec2f201d32ac854a6d7236
liuchanglu <ChangLu_Liu@163.com>
2019-11-19 21:18:45 +0800

    Initial commit

---
+++ src/main/java/com/langu/Main.java
@@ -0,0 +1,33 @@
+package com.langu;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * @author 刘常禄
+ * @version 1.0
+ * @data 2019-11-19 20:29
+ */
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+
+
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setTitle("javaFx");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
